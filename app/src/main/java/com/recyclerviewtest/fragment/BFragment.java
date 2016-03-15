c7356197686f2a45ea5da54ef308cd03522b104e
Next-discover <769170220@qq.com>
2016-03-15 23:17:11 +0800

    更换gradview,和瀑布流

--- app/src/main/java/com/recyclerviewtest/fragment/BFragment.java
+++ app/src/main/java/com/recyclerviewtest/fragment/BFragment.java
@@ -1,13 +1,24 @@
 package com.recyclerviewtest.fragment;
 
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.recyclerviewtest.R;
+import com.recyclerviewtest.adapter.RecyclerViewAdapter;
 import com.recyclerviewtest.base.BaseFragment;
+import com.recyclerviewtest.util.RecycleViewDivider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/3/10.
  */
 public class BFragment extends BaseFragment {
 
+    private RecyclerView listB;
+    private List<String> mDatas;
 
     @Override
     protected int setContentViewId() {
@@ -16,7 +27,12 @@ public class BFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        listB = (RecyclerView)findViewById(R.id.list_b);
+        GridLayoutManager llManager =  new GridLayoutManager(getActivity(),3);
+        listB.setLayoutManager(llManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity(),mDatas);
+        listB.setAdapter(adapter);
+        listB.addItemDecoration(new RecycleViewDivider(getActivity(), LinearLayoutManager.VERTICAL));
     }
 
     @Override
@@ -26,6 +42,10 @@ public class BFragment extends BaseFragment {
 
     @Override
     protected void initGetData() {
-
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("" + (char) i);
+        }
     }
 }
--- app/src/main/java/com/recyclerviewtest/fragment/CFragment.java
+++ app/src/main/java/com/recyclerviewtest/fragment/CFragment.java
@@ -1,14 +1,25 @@
 package com.recyclerviewtest.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
 import com.recyclerviewtest.R;
+import com.recyclerviewtest.adapter.RecyclerViewAdapter;
 import com.recyclerviewtest.base.BaseFragment;
+import com.recyclerviewtest.util.RecycleViewDivider;
+import com.recyclerviewtest.util.SpacesItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/3/10.
  */
 public class CFragment extends BaseFragment {
 
-
+    private RecyclerView listC;
+    private List<String> mDatas;
     @Override
     protected int setContentViewId() {
         return R.layout.fragment_c;
@@ -16,7 +27,15 @@ public class CFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        listC = (RecyclerView)findViewById(R.id.list_c);
+        StaggeredGridLayoutManager llManager =  new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        listC.setLayoutManager(llManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity(),mDatas);
+        listC.setAdapter(adapter);
+        listC.addItemDecoration(new RecycleViewDivider(getActivity(), LinearLayoutManager.VERTICAL));
 
+        SpacesItemDecoration decoration=new SpacesItemDecoration(16);
+        listC.addItemDecoration(decoration);
     }
 
     @Override
@@ -26,6 +45,10 @@ public class CFragment extends BaseFragment {
 
     @Override
     protected void initGetData() {
-
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("" + (char) i);
+        }
     }
 }
